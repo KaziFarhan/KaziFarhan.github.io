db07cd47d410d8753358d80ed4870074b0511324
KaziFarhanuddin <kazifarhan199@gmail.com>
2020-04-08 02:06:31 +0530

    Added table of content in nav for pages that have h3 tags

--- js/myjs.js
+++ js/myjs.js
@@ -1,26 +1,18 @@
-// Index page load
-let theme = localStorage.getItem('theme')
-console.log(theme)
-if (theme){
-    document.getElementById('light-css').removeAttribute('disabled')
-    document.getElementById('dark-css').setAttribute('disabled', true);
-}
-
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = function() { 
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-        var Page = marked(xmlHttp.responseText);
-        document.getElementById('content').innerHTML = Page;
-        document.getElementById('content').querySelectorAll('code').forEach(element => {
-            hljs.highlightBlock(element);
-        });
-        document.getElementById('content').querySelectorAll('.pagelink').forEach(element => {
-            element.addEventListener('click', changePage)
-        });
+// Nav bar working 
+let content = document.getElementById('content');
+var navigaTion = document.getElementById('navigation');
+content.addEventListener('click', ()=>
+    navigaTion.style.display = 'none'
+)
+function toggleNavBar() {
+    if (navigaTion.style.display=='block'){
+        navigaTion.style.display = 'none';
+    }
+    else{
+        navigaTion.style.display = 'block';
     }
 }
-xmlHttp.open("GET", 'index.md', true); // true for asynchronous 
-xmlHttp.send(null);
+
 
 // Nav bar build
 var navXmlHttp = new XMLHttpRequest();
@@ -40,7 +32,6 @@ navXmlHttp.onreadystatechange = function() {
                     localStorage.setItem('theme', 'light')
                     document.getElementById('light-css').removeAttribute('disabled')
                     document.getElementById('dark-css').setAttribute('disabled', true);
-
                 }
             }
         )
@@ -49,20 +40,41 @@ navXmlHttp.onreadystatechange = function() {
 navXmlHttp.open("GET", 'nav.md', true);
 navXmlHttp.send(null);
 
-// Nav bar working 
-let content = document.getElementById('content');
-var navigaTion = document.getElementById('navigation');
-content.addEventListener('click', ()=>
-    navigaTion.style.display = 'none'
-)
-function toggleNavBar() {
-    if (navigaTion.style.display=='block'){
-        navigaTion.style.display = 'none';
-    }
-    else{
-        navigaTion.style.display = 'block';
+
+// Index page load
+let theme = localStorage.getItem('theme')
+console.log(theme)
+if (theme){
+    document.getElementById('light-css').removeAttribute('disabled')
+    document.getElementById('dark-css').setAttribute('disabled', true);
+}
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        var Page = marked(xmlHttp.responseText);
+        document.getElementById('content').innerHTML = Page;
+        document.getElementById('content').querySelectorAll('code').forEach(element => {
+            hljs.highlightBlock(element);
+        });
+        document.getElementById('content').querySelectorAll('.pagelink').forEach(element => {
+            element.addEventListener('click', changePage)
+        });
+        let tableOfContent = '';
+        document.getElementById('content').querySelectorAll('h3').forEach(element => {
+            tableOfContent += `<li class="link"><a href="#${element.id}">${element.innerText}</a></li>`
+        });
+        if (!tableOfContent){
+            document.getElementById('tableofcontentnav').style.display='none';
+        }
+        else{
+            document.getElementById('tableofcontentnav').style.display='block';
+        }
+        document.getElementById('tableofcontent').innerHTML = tableOfContent
     }
 }
+xmlHttp.open("GET", 'index.md', true); // true for asynchronous 
+xmlHttp.send(null);
 
 // Change Page
     navigaTion.addEventListener('click', changePage)
--- nav.md
+++ nav.md
@@ -2,4 +2,8 @@
 <li class="link" data-file='about.md'>About</li>
 <li class="link" data-file='plan.md'>My Plans</li>
 <div id="changeTheme">Change Theme</div>
-
+<div id='tableofcontentnav'>
+    <br>  
+    Table of Content
+    <ul id="tableofcontent"></ul>
+</div>
